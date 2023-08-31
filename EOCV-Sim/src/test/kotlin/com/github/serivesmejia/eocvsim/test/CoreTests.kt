6f08f85e9aaa151453a5a2113432668bd00123c9
serivesmejia <serivesmejia@gmail.com>
2023-08-31 07:49:18 -0700

    ADd AprilTags Unit Test

--- EOCV-Sim/src/test/kotlin/com/github/serivesmejia/eocvsim/test/CoreTests.kt
+++ EOCV-Sim/src/test/kotlin/com/github/serivesmejia/eocvsim/test/CoreTests.kt
@@ -27,6 +27,7 @@ package com.github.serivesmejia.eocvsim.test
 import com.github.serivesmejia.eocvsim.EOCVSim
 import io.kotest.core.spec.style.StringSpec
 import org.opencv.core.Mat
+import org.openftc.apriltag.AprilTagDetectorJNI
 
 class OpenCvTest : StringSpec({
     "Loading native library" {
@@ -35,5 +36,16 @@ class OpenCvTest : StringSpec({
 
     "Creating a Mat" {
         Mat()
+    }
+})
+
+class AprilTagsTest : StringSpec({
+    "Create AprilTag detector" {
+        val detector = AprilTagDetectorJNI.createApriltagDetector(
+                AprilTagDetectorJNI.TagFamily.TAG_36h11.string,
+                0f, 3
+        )
+
+        println("Created detector $detector")
     }
 })
